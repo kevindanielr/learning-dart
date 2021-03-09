fca61cfce43f26a6999f38703b6ac4e467e8f033
kevindanielr <kevindanielr@gmail.com>
2021-03-09 16:01:34 -0600

    proyecto componentes

---
+++ componentes/lib/src/pages/home_temp.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItemCorta(),
+      ),
+    );
+  }
+
+  // Metodo que regresa una lista de Widget
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      // Ingresando Widget a la lista
+      lista
+        ..add(tempWidget)
+        ..add(Divider(
+          height: 2,
+        ));
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            // leading: Icon(Icons( Icons.north_sharp )),
+          ),
+          Divider(height: 2)
+        ],
+      );
+    }).toList();
+  }
+}
