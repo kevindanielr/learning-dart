244c54d345bf0dc6c97e8894a68cdce98af3d6a2
kevindanielr <kevindanielr@gmail.com>
2021-03-11 15:40:32 -0600

    finish appComponent

--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -7,6 +7,8 @@ import 'package:componentes/src/pages/home._page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
+import 'package:componentes/src/pages/listview_page.dart';
 
 // Metodo que retorna Rutas a Main
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -17,5 +19,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage(),
+    'list': (BuildContext context) => ListaPage(),
   };
 }
